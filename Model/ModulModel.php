24b6cb483e64119758c3c7cfed14248fa3d6b67a
mqadri05 <muhqadry0504@gmail.com>
2021-05-19 22:11:49 +0700

    8.1.1-8.1.4

--- Model/ModulModel.php
+++ Model/ModulModel.php
@@ -18,5 +18,72 @@ class ModulModel
 		extract($data);
 		require_one("View/modul/index.php");
 	}
+		/**
+		*function prosesstore berfungsi input data praktikum
+		*@param String $modul berisi nama modul praktikum
+		*@param String $idPraktikum berisi id praktikum
+   	**/
+   	public function prosesStore($modul, $idPraktikum)
+   	{
+   		$sql="INSERT INTO modul(nama,praktikum_id) VALUES('$modul','$idPraktikum')";
+   		return koneksi()->query($sql);
+   	}
+   	public function prosesDelete($id)
+   	{
+   		$sql="DELETE FROM modul WHERE id=$id";
+   		return koneksi()->query($sql);
+   	}
+   	public function getPraktikum()
+   	{
+   		$sql="SELECT * from praktikum WHERE status=1";
+   		$query=koneksi()->query($sql);
+   		$hasil= [];
+   		while ($data= $query->fetch_assoc()){
+   			$hasil[]= $data;
+   		}
+   		return $hasil;
+   	}
+   	public function create()
+
+   	{
+   		$data= $this->getPraktikum();
+   		extract($data);
+   		require_once("View/modul/create.php");
+   	}
+   	public function store()
+   	{
+   		$modul= $_POST['modul'];
+   		$praktikum= $_POST['praktikum'];
+   		$getLastData = $this->getLastData();
+   		if(getLastData== null) {
+   			for($i=1;$i= <= $modul; $i++){
+   				$nama = 'Modul'.$i;
+   				$post = $this->prosesStore($nama,$praktikum);
+   			}
+   	}
+   	else {
+   		$modulLast= explode(" ", $getLastData['nama']);
+   		for($i=1;$i= <= $modul; $i++){
+   				$a = $modulLast['1'] +=1;
+   				$nama = 'Modul' . $a;
+   				$post = $this->prosesStore($nama,$praktikum);
+   			}
+   	}
+   		if ($post){
+   			header("location:index.php?page=modul&aksi=view&pesan=BerhasilMenambahData");
+   		}
+   		else {
+   			header("location:index.php?page=modul&aksi=create&pesan=GagalMenambahData");
+   		}
+   }
+   public function delete()
+   	{
+   		$id=$_GET['id'];
+   		if(this->prosesDelete($id)){
+   			header("location:index.php?page=modul&aksi=view&pesan=BerhasilDeleteData");
+   		}else{
+   			header("location:index.php?page=modul&aksi=view&pesan=GagalDeleteData");
+   		}
+   	}
 }
 ?>
--- Model/PraktikumModel.php
+++ Model/PraktikumModel.php
@@ -21,6 +21,77 @@ class PraktikumModel
    		extract($data);
    		require_once("View/praktikum/index.php");
    	}
+   	/**
+		*function prosesstore berfungsi input data praktikum
+		*@param String $nama berisi nama praktikum
+		*@param String $tahun berisi tahun praktikum
+   	**/
+   	public function prosesStore($nama, $tahun)
+   	{
+   		$sql="INSERT INTO praktikum(nama,tahun) VALUES('$nama','$tahun')";
+   		return koneksi()->query($sql);
+   	}
+   	public function storeUpdate($nama, $tahun, $id)
+   	{
+   		$sql="UPDATE praktikum SET nama='$nama',tahun='$tahun' WHERE id=$id";
+   		return koneksi()->query($sql);
+   	}
+   		public function prosesAktifkan($id)
+   	{
+   		koneksi()->query(("UPDATE praktikum SET status=0"));
+   		$sql="UPDATE praktikum SET status=1 WHERE id=$id";
+   		return koneksi()->query($sql);
+   	}
+   		public function create()
+
+   	{
+   		require_once("View/praktikum/create.php");
+   	}
+   	public function store()
+   	{
+   		$nama= $_POST['nama'];
+   		$tahun= $_POST['tahun'];
+   		if(this->prosesStore('$nama','$tahun')){
+   			header("location:index.php?page=praktikum&aksi=view&pesan=BerhasilMenambahData");
+   		}else{
+   			header("location:index.php?page=praktikum&aksi=create&pesan=GagalMenambahData");
+   		}
+   	}
+   	public function getById($id)
+   	{
+   		$sql="SELECT * from praktikum WHERE id=$id";
+   		$query=koneksi()->query($sql);
+   		return koneksi()->fetch_assoc();
+   	}
+   	public function update()
+   	{
+   		$id=$_POST['id'];
+   		$nama= $_POST['nama'];
+   		$tahun= $_POST['tahun'];
+   		if(this->storeUpdate('$nama','$tahun' ,'id')){
+   			header("location:index.php?page=praktikum&aksi=view&pesan=BerhasilMengubahData");
+   		}else{
+   			header("location:index.php?page=praktikum&aksi=edit&pesan=GagalMengubahData&id=$id");
+   		}
+   	}
+   	public function aktifkan()
+   	{
+   		$id=$_GET['id'];
+   		if(this->prosesAktifkan($id)){
+   			header("location:index.php?page=praktikum&aksi=view&pesan=BerhasilMen-AktifkanData");
+   		}else{
+   			header("location:index.php?page=praktikum&aksi=edit&pesan=GagalMen-AktifkanData&id=$id");
+   		}
+   	}
+   	 	public function nonAktifkan()
+   	{
+   		$id=$_GET['id'];
+   		if(this->prosesNonAktifkan($id)){
+   			header("location:index.php?page=praktikum&aksi=view&pesan=Berhasilnon-AktifkanData");
+   		}else{
+   			header("location:index.php?page=praktikum&aksi=edit&pesan=Gagalnon-AktifkanData&id=$id");
+   		}
+   	}
 }
 
 ?>
